3f362f539c2cbfe9b4da8dccd8f0b2a618d7d4df
3e-x <abbas.deebajah@gmail.com>
2025-10-23 19:56:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -501,6 +501,8 @@
         'SSOC Reason',
         'Action Taken - Consumer',
         'SSOC incident source'
+		'City',
+		'Country'
     ];
 
     // Check if a field is a system field that should never be hidden (Requester, Assignee, CCs)
